937d7597fcfc4d87858ea0a1ebf615cbaedcbf6b
Mariya <33662721+MariyaGiordani@users.noreply.github.com>
2019-02-27 09:52:49 -0300

    Add files via upload

---
+++ RecursiveFifo/RecursiveArray/CalcMean.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RecursiveArray
+{
+
+    // To find the mean of the elements of the array.
+    // Mean = (Sum of elements of the Array) / (Total no of elements in Array)
+    // Examples:
+    // Input : 4 6 3 4 5
+    // Output : 3
+
+    class CalcMean
+    {
+        static float findMean(int[] A, int N)
+        {
+            if (N == 1)
+            {
+                return (float)A[N - 1];
+            }
+            else
+            {
+                return ((float)(findMean(A, N - 1) * (N - 1) + A[N - 1]) / N);
+            }
+        }
+
+        public static void Main()
+        {
+            int[] A = { 4, 6, 3, 4, 5 };
+            int N = A.Length;
+            Console.WriteLine("Input : 4 6 3 4 5 ");
+            Console.WriteLine("Mean = (Sum of elements of the Array) / (Total no of elements in Array)");
+            float result = findMean(A, N);
+            Console.WriteLine("Output : " + result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RecursiveFifo/RecursiveFifoExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFifoExample
+{
+    class Example
+    {
+        static int Fifo(int[] v, int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int s;
+                Console.WriteLine(" index = " + n);
+                s = Fifo(v, n - 1);
+                Console.WriteLine(" index = " + (n - 1));
+
+                if (v[n - 1] > 0)
+                {
+                    Console.WriteLine(" Enter in the condition index [" + (n - 1) + "]");
+                    Console.WriteLine(" v = " + v[n - 1]);
+                    Console.WriteLine(" s = index [" + (n - 1) + "]");
+                    Console.WriteLine(" s = s + v ");
+                    Console.WriteLine(" s = " + s + " + " + v[n - 1]);
+                    s = s + v[n - 1];
+                    Console.WriteLine(" s  = " + s);
+                }
+                return s;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 2, -4, 7, 0, -1, 4 };
+            int number = 6;
+            var result = Fifo(array, number);
+            Console.WriteLine("Result of s = " + result);
+            Console.ReadKey();
+        }
+    }
+}
